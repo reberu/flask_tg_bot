673fe4ffad879d5d233c91470372bcf15fd04107
reberu <redlightexit@gmail.com>
2021-08-22 22:11:31 +0900

    Добавлены главная страница, страница авторизации и страница админки. Реализована аутентификация пользователей из таблицы admins, где пароли захешированы модулем werkzeug.security. Реализованы формы аутентификации, добавления блюда и добавления категории.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from settings import Config
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask_admin import Admin
@@ -16,4 +17,6 @@ app.register_blueprint(dishes, url_prefix='/dishes')
 manager = Manager(app)
 db = SQLAlchemy(app)
 sched = BackgroundScheduler()
-admin = Admin(app)
+# admin = Admin(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
