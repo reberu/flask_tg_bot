8555322cc11958e7c1f580aefc56e826299c34ba
reberu <redlightexit@gmail.com>
2023-02-21 17:04:19 +0900

    Added text handlers and fixed restaurant handlers

--- views.py
+++ views.py
@@ -1,16 +1,21 @@
-import datetime
+from datetime import datetime
 from itertools import chain
+from os import mkdir
+from os.path import isdir
+from time import sleep
 
 import pytz
 import telebot.types
+from PIL import Image
 from flask import render_template, flash, redirect, url_for, Response
 from sqlalchemy import func
 from sqlalchemy.util import symbol
 
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, Update, WebAppInfo
+from telebot.util import antiflood
 
 from handlers import restaurant_callback, cart_callback, order_callback, other_callback, favorites_callback
-from utils import check_user, write_history, rest_menu_keyboard
+from utils import check_user, write_history, rest_menu_keyboard, stat5, stat8, stat7, stat6, stat4, stat3, stat2, stat1
 
 from forms import LoginForm, DishForm, CategoryForm, DishDeleteForm, RestaurantForm, CategoryDeleteForm, \
     RestaurantDeleteForm, RestaurantEditForm, AdminAddForm, RestaurantDeliveryTermsForm, \
@@ -69,7 +74,40 @@ def stat_menu_keyboard(message):
 
 
 def default_message(message):
-    text = 'DEFAULT TEXT'
+    text = 'Не могу найти то, что Вы ищете🧐 Попробуйте изменить запрос😊'
+    result = None
+    for word in message.text:
+        result = db.session.query(SearchWords.id).filter(SearchWords.words.ilike("%" + word + "%")).first()
+    if result:
+        query = db.session.query(Category.id, Restaurant.name, Dish.img_link, SearchDishes.dish_name, Dish.composition,
+                                 Dish.cost, Dish.id, Restaurant.id).filter(
+            SearchDishes.search_words_id == result.id).filter(
+            Restaurant.id == SearchDishes.rest_id, Dish.id == SearchDishes.dish_id).filter(
+            Category.name == SearchDishes.dish_category, Category.restaurant_id == SearchDishes.rest_id).all()
+        for item in query:
+            text = f'{item[1]}\n<a href="{item[2]}">.</a>\n{item[3]}\n{item[4]}\n{item[5]} р.'
+            cart = Cart.query.filter_by(user_uid=message.chat.id, dish_id=item[6]).first()
+            quantity = cart.quantity if cart else 0
+            cb_data = f'rest_{item[7]}_cat_{item[0]}_dish_{item[6]}'
+            cb_fav = f'fav_{message.chat.id}_{item[7]}_{item[6]}'
+            kbd = InlineKeyboardMarkup()
+            kbd.row(
+                InlineKeyboardButton(text='⭐️', callback_data=cb_fav),
+                InlineKeyboardButton(text='-', callback_data=f'{cb_data}_rem_{message.chat.id}'),
+                InlineKeyboardButton(text=f'{quantity} шт', callback_data='None'),
+                InlineKeyboardButton(text='+', callback_data=f'{cb_data}_add_{message.chat.id}')
+            )
+            total = db.session.query(func.sum(Cart.price * Cart.quantity)).filter_by(user_uid=message.chat.id).all()
+            total = total[0][0] if total[0][0] else 0
+            kbd.add(InlineKeyboardButton('Меню ресторана', callback_data=f'rest_{item[7]}_menu'))
+            kbd.add(InlineKeyboardButton(f'В корзину: заказ на сумму {total} р.', callback_data='cart'))
+            BOT.send_message(chat_id=message.chat.id, text=text, parse_mode='HTML', reply_markup=kbd)
+        return 'Ok', 200
+    result = Restaurant.query.filter_by(passwd=message.text).first()
+    if result:
+        text = f'Вы назначены администратором ресторана {result.name}'
+        result.service_uid = message.chat.id
+        db.session.commit()
     BOT.send_message(chat_id=message.chat.id, text=text)
 
 
@@ -80,11 +118,11 @@ def webhook():
     try:
         check_user(update.message.json)
         write_history(
-                msg_id=update.message.json["message_id"],
-                chat_id=update.message.json["chat"]["id"],
-                text=update.message.json["text"],
-                is_bot=False
-            )
+            msg_id=update.message.json["message_id"],
+            chat_id=update.message.json["chat"]["id"],
+            text=update.message.json["text"],
+            is_bot=False
+        )
     except AttributeError:
         pass
     return 'Ok', 200
@@ -94,7 +132,7 @@ def webhook():
 def start(message):
     """Обработка команды /start"""
     text = "*Добро пожаловать в Robofood*😊\n" \
-           "Здесь `Вы` можете заказать еду из ресторанов на доставку и самовывоз. "\
+           "Здесь `Вы` можете заказать еду из ресторанов на доставку и самовывоз. " \
            "Начните с кнопки “Меню” или наберите в сообщении интересующую еду 🍱🥤🍕 "
     BOT.send_message(message.chat.id, text, parse_mode="MARKDOWN")
 
@@ -108,7 +146,7 @@ def user_orders(message):
         BOT.send_message(message.chat.id, text, parse_mode="MARKDOWN")
         return 'Ok', 200
     date = order.order_datetime
-    date = YKT.localize(datetime.datetime.fromtimestamp(date)).strftime('%d.%m.%Y %H:%M:%S')
+    date = YKT.localize(datetime.fromtimestamp(date)).strftime('%d.%m.%Y %H:%M:%S')
     text = f'Ваш заказ № {order.id} от {date}\n- '
     details = OrderDetail.query.filter_by(order_id=order.id).all()
     text += '- '.join("%s\n" % item.order_dish_name for item in details)
@@ -138,13 +176,12 @@ def combo(message):
     for item in kb_parsed:
         rests.append(item.text)
     combo_dishes = db.session.query(
-            Dish, Restaurant, SpecialDish
-        ).filter(SpecialDish.subcat_id == -1).filter(
-            SpecialDish.dish_id == Dish.id, SpecialDish.rest_id == Restaurant.id
-        ).all()
+        Dish, Restaurant, SpecialDish
+    ).filter(SpecialDish.subcat_id == -1).filter(
+        SpecialDish.dish_id == Dish.id, SpecialDish.rest_id == Restaurant.id
+    ).all()
     cart = Cart.query.filter_by(user_uid=message.chat.id).all()
     for item in combo_dishes:
-        """item[0] - Dish object, item[1] - Restaurant object"""
         keyboard = InlineKeyboardMarkup(row_width=4)
         text = ''
         text += f'<b>Ресторан {item[1].name}</b>'
@@ -262,8 +299,6 @@ def send_help(message):
 @BOT.message_handler(content_types=["text"])
 def new_msg(message):
     """Обработка текстовых сообщений"""
-    if message == 'Укажите только адрес доставки для ресторана Суши Весла 11:00-23:00':
-        print('gotcha')
     options = {
         "Рестораны": rest_menu_send_msg,
         "Комбо Наборы (КБ)": combo,
@@ -272,10 +307,6 @@ def new_msg(message):
         "Корзина": show_cart,
         "Статистика": stat_menu_keyboard,
     }
-    try:
-        print(message.message_id)
-    except Exception as e:
-        print(e)
     options.get(message.text, default_message)(message)
 
 
@@ -303,3 +334,136 @@ def index():
 @login_manager.user_loader
 def load_user(user_id):
     return db.session.query(Admin).get(user_id)
+
+
+@app.route('/login/', methods=['POST', 'GET'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('admin'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = db.session.query(Admin).filter(Admin.username == form.username.data).first()
+        if user and user.verify_password(form.password.data):
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('admin'))
+
+        flash("Invalid username/password", 'error')
+        return redirect(url_for('login'))
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout/')
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out.")
+    return redirect(url_for('login'))
+
+
+@app.route('/statistics', methods=['GET'])
+@login_required
+def statistics():
+    return render_template(
+        'statistics.html'
+    )
+
+
+@app.route('/statistics/1', methods=['GET'])
+@login_required
+def statistics1():
+    return render_template('stat1.html', stat1=stat1())
+
+
+@app.route('/statistics/2', methods=['GET'])
+@login_required
+def statistics2():
+    return render_template('stat2.html', stat2=stat2())
+
+
+@app.route('/statistics/3', methods=['GET'])
+@login_required
+def statistics3():
+    return render_template('stat3.html', stat3=stat3())
+
+
+@app.route('/statistics/4', methods=['GET'])
+@login_required
+def statistics4():
+    return render_template('stat4.html', stat4=stat4())
+
+
+@app.route('/statistics/5', methods=['GET'])
+@login_required
+def statistics5():
+    return render_template('stat5.html', stat5=stat5()[0])
+
+
+@app.route('/statistics/6', methods=['GET'])
+@login_required
+def statistics6():
+    return render_template('stat6.html', stat6=stat6())
+
+
+@app.route('/statistics/7', methods=['GET'])
+@login_required
+def statistics7():
+    return render_template('stat7.html', stat7=stat7())
+
+
+@app.route('/statistics/8', methods=['GET'])
+@login_required
+def statistics8():
+    return render_template('stat8.html', stat8=stat8())
+
+
+@app.route('/database', methods=['GET', 'POST'])
+@login_required
+def get_database():
+    date_form = DateForm()
+    date = datetime.now().strftime('%Y-%m-%d')
+    if date_form.is_submitted():
+        date = request.form['input_date']
+        date_start = date + '-0-0'
+        date_end = date + '-23-59'
+        date_start_format = datetime.strptime(date_start, "%Y-%m-%d-%H-%M")
+        date_end_format = datetime.strptime(date_end, "%Y-%m-%d-%H-%M")
+
+        unix_time_start = datetime.timestamp(date_start_format)
+        unix_time_end = datetime.timestamp(date_end_format)
+
+        history_data = db.session.query(History.chat_id).filter(
+            History.date.between(unix_time_start, unix_time_end)).distinct().all()
+        data = [i for sub in history_data for i in sub]
+        users = User.query.filter(User.uid.in_(data)).all()
+        return render_template('database.html', date_form=date_form, users=users, orders=Order.query.all(),
+                               details=OrderDetail.query.all(), unique=stat5()[1], date=date)
+    else:
+        return render_template('database.html', date_form=date_form, date=date)
+
+
+@app.route('/send_message', methods=['GET', 'POST'])
+@login_required
+def send_message():
+    if request.method == 'POST':
+        users = User.query.all()
+        img_file = request.files['msg_img']
+        temp_path = 'tmp/'
+        if not isdir(temp_path):
+            mkdir(temp_path)
+        path = temp_path + secure_filename(img_file.filename)
+        with Image.open(img_file) as img:
+            width, height = img.size
+            resized_dimensions = (int(width * 0.5), int(height * 0.5))
+            resized = img.resize(resized_dimensions)
+            resized.save(path, format='png')
+            for user in users:
+                msg = antiflood(BOT.send_photo, chat_id=user.uid, photo=open(path, 'rb'),
+                                caption=request.form.get('msg_txt'))
+        return redirect(url_for('send_message'))
+        # TODO remove files in tmp after sending
+    return render_template('send_message.html')
+
+
+@app.route('/contract/', methods=['GET'])
+def contract():
+    return render_template('contract.html', contract_text=contract_text)
